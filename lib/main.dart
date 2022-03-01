15cd15dfb3673569a96af1895d6285de84630e12
shubham0467 <palshubham25@gmail.com>
2022-03-01 20:45:35 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:biaom_app/login_page.dart';
+import 'package:BOAIAM/provider/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:admob_flutter/admob_flutter.dart';
+import 'package:provider/provider.dart';
+import 'Page/home_page.dart';
+//import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,11 +12,26 @@ void main() async {
   Admob.initialize();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "BIAOM",
-      home: LoginDemo(),
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Light & Dark Theme';
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+
+          return MaterialApp(
+            title: "BIAOM",
+            debugShowCheckedModeBanner: false,
+            themeMode: themeProvider.themeMode,
+            theme: MyThemes.lightTheme,
+            darkTheme: MyThemes.darkTheme,
+            home: HomePage(),
+          );
+        },
+      );
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,16 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:biaom_app/course_detail.dart';
+import 'package:BOAIAM/Page/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:biaom_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const course());
+    await tester.pumpWidget(const HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
